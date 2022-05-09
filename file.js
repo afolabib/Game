2b3a4a05b7a35c675e267e8432606a6b75179346
afolabib <afolabib@tcd.ie>
2022-05-09 14:14:46 +0000

    ensure js is working well with html and fix bugs

--- file.js
+++ file.js
@@ -4,14 +4,13 @@ const userscore_span = document.getElementById("user_score");
 const compscore_span = document.getElementById("comp_score");
 const score_board = document.querySelector(".score_board");
 const outcome_div = document.querySelector(".outcome > p");
-const rock_p = document.getElementById("rock");
+const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 const choices_Array = {'r':'Rock','p':'Paper','s':'Scissors'};
 
-
 function getComputerChoice() {
-    const choices = ['r', 'p' , 's';
+    const choices = ['r', 'p' , 's'];
     const randomNumber = Math.floor(Math.random() *3);
     return choices[randomNumber]
 }
@@ -20,34 +19,35 @@ function win(userChoice, ComputerChoice) {
     userscore++;    
     userscore_span.innerHTML = userscore
     compscore_span.innerHTML =compscore;
-    outcome_div.innerHTML = choices_Array[userChoice] + " beats " + choices_Array[ComputerChoice]+'. You Win.'
+    outcome_div.innerHTML = choices_Array[userChoice] + " beats " + choices_Array[ComputerChoice]+'. You Win.'  
 }
 
 function lose(userChoice, ComputerChoice) {
     compscore++;
     userscore_span.innerHTML = userscore
     compscore_span.innerHTML =compscore;
-    outcome_p.innerHTML = userChoice + "beats" + ComputerChoice
-    
+    outcome_div.innerHTML = choices_Array[ComputerChoice] + " beats " + choices_Array[userChoice]+'. You Lose.'   
 }
 
 function draw(userChoice, ComputerChoice) {
     userscore_span.innerHTML = userscore
     compscore_span.innerHTML =compscore;
-    outcome_p.innerHTML = userChoice + "beats" + ComputerChoice
-    
+    outcome_div.innerHTML = 'Draw'
 }
+
 function game(userChoice) {
     const ComputerChoice = getComputerChoice();
     switch (userChoice + ComputerChoice) {
         case "pr":
         case "rs":
         case "sp":
+            console.log("win")
             win(userChoice, ComputerChoice)
             break;
         case "rp":
         case "sr":
         case "ps":
+            console.log("lose")
             lose(userChoice, ComputerChoice)
             break;
         case "rr":
@@ -63,17 +63,16 @@ function game(userChoice) {
 
 }
 
-function main() {
+
     rock_div.addEventListener('click', function() {
-        game("rock");
-}) 
+        game("r");
+    }) 
 
     paper_div.addEventListener('click', function() {
-        game("paper");
-}) 
+        game("p");
+    }) 
 
     scissors_div.addEventListener('click', function() {
-        game("rock");
-}) 
-}
+        game("s");
+    }) 
 
